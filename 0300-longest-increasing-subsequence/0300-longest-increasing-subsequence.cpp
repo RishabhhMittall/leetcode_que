91926c91f9e2613b384af3a5e886fecfb26ab6a4
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-05 19:19:05 +0530

    Time: 3 ms (98.69%), Space: 10.6 MB (92.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
+
+    // int solve(vector<int> nums, int n , int curr, int prev){
+    //     if(curr == n) return 0;
+
+    //     //include
+    //     int take = 0;
+    //     if(prev == -1 || nums[curr] > nums[prev]) take = 1 + solve(nums, n, curr +1, curr);
+
+    //     //exclude
+    //     int notTake = 0 + solve(nums, n, curr+1, prev);
+    // }
     int lengthOfLIS(vector<int>& nums) {
+        //---Approach 1 Binary search-----
         int arr[nums.size()];
         int size = 0;
         for(int x : nums) {
@@ -20,5 +32,25 @@ public:
             
         }
         return size;
+// Approach 2 --- DP----
+    //    if (nums.empty()) return 0;
+        
+    //     int n = nums.size();
+    //     vector<int> dp(n, 1); 
+        
+    //     int maxLen = 1;
+    //     for (int i = 1; i < n; ++i) {
+    //         for (int j = 0; j < i; ++j) {
+    //             if (nums[i] > nums[j]) {
+    //                 dp[i] = max(dp[i], dp[j] + 1);
+    //                 maxLen = max(maxLen, dp[i]);
+    //             }
+    //         }
+    //     }
+    //     return maxLen;
+
+// Approach 3 ----recurssion(TLE)----
+    // int n = nums.size();
+    // return solve(nums,n,0,-1);
     }
 };
