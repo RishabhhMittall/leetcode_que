7fe3a20e5405102fe4da7485352f7c0b1cb01f39
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-08 12:48:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -3,29 +3,29 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         
         int n = nums.size();
-        int s = 0, e = n-1;
+        // int s = 0, e = n-1;
         
 
-        while(s<e){
-            int mid = s +(e-s)/2;
-            // if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
-            //     return nums[mid];
-            // }
-            if(mid % 2 != 0) mid --;
+        // while(s<e){
+        //     int mid = s +(e-s)/2;
+        //     // if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+        //     //     return nums[mid];
+        //     // }
+        //     if(mid % 2 != 0) mid --;
             
-            if (nums[mid] != nums[mid+1]) e = mid;
-            else if(nums[mid] == nums[mid+1]){ 
-                s = mid+2;
-            }
+        //     if (nums[mid] != nums[mid+1]) e = mid;
+        //     else if(nums[mid] == nums[mid+1]){ 
+        //         s = mid+2;
+        //     }
             
              
              
-        }
-
-        // for(int i = 0; i<n; i++){
-        //     if(nums[i] == nums[i+1]) i++;
-        //     else return nums[i];
         // }
-        return nums[s] ;
+
+        for(int i = 0; i<n; i++){
+            if(nums[i] == nums[i+1]) i++;
+            else return nums[i];
+        }
+        return -1 ;
     }
 };
