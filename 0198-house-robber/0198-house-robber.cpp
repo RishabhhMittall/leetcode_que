cfaddfc7a8abb6c3577d317674835402cca1ae32
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-21 16:02:36 +0530

    Time: 4 ms (25.45%), Space: 9 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,29 +1,23 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        
+
         if (n == 0) {
             return 0;
-        } else if (n == 1) {
+        } 
+        if (n == 1) {
             return nums[0];
         }
+        int premax = 0; 
+        int curmax = 0; 
 
-        // Initialize two variables to keep track of the maximum money robbed
-        int prevMax = 0; // maximum money robbed up to the previous house
-        int currMax = 0; // maximum money robbed up to the current house
-
-        for (int i = 0; i < n; ++i) {
-            // Calculate the new maximum money robbed up to the current house
-            int temp = currMax;
-            currMax = max(currMax, prevMax + nums[i]);
-            prevMax = temp;
+        for (int i = 0; i < n; i++) {
+            int temp = curmax;
+            curmax = max(curmax, premax + nums[i]);
+            premax = temp;
         }
 
-        // The final result is stored in currMax
-        return currMax;
+        return curmax;
     }
 };
