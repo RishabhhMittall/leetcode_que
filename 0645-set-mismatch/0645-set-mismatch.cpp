9d1ddc5010983e7fc32dd66ee6724767c603ffae
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-22 10:38:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -4,19 +4,23 @@ public:
         vector<int> ans;
         int n = nums.size();
         
-        // Sort the vector to simplify the search for duplicated numbers
         sort(nums.begin(), nums.end());
+        // for (int i = 0; i < n - 1; i++) {
+        //     if (nums[i] == nums[i + 1]) {
+        //         ans.push_back(nums[i]);  // Duplicated number
+        //     }
+        // }
         
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                ans.push_back(nums[i]);  // Duplicated number
-            }
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            if (find(nums.begin(), nums.end(), i) == nums.end()) {
-                ans.push_back(i);  // Missing number
-                break;
+        // for (int i = 1; i <= n; i++) {
+        //     if (find(nums.begin(), nums.end(), i) == nums.end()) {
+        //         ans.push_back(i);  // Missing number
+        //         break;
+        //     }
+        // }
+        for(int i = 0; i<n; i++){
+            if(nums[i] != i + 1){
+                ans.push_back(nums[i]);
+                ans.push_back(i + 1);
             }
         }
         
