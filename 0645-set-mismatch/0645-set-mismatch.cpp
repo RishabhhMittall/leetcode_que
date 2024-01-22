4f6d38d8ae9a1556346cd9747571ed39874d48c7
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-22 10:59:43 +0530

    Time: 29 ms (53.42%), Space: 24.8 MB (34.75%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -7,23 +7,61 @@ public:
         sort(nums.begin(), nums.end());
         // for (int i = 0; i < n - 1; i++) {
         //     if (nums[i] == nums[i + 1]) {
-        //         ans.push_back(nums[i]);  // Duplicated number
+        //         ans.push_back(nums[i]);
         //     }
         // }
         
         // for (int i = 1; i <= n; i++) {
         //     if (find(nums.begin(), nums.end(), i) == nums.end()) {
-        //         ans.push_back(i);  // Missing number
+        //         ans.push_back(i); 
         //         break;
         //     }
         // }
-        for(int i = 0; i<n; i++){
-            if(nums[i] != i + 1){
-                ans.push_back(nums[i]);
-                ans.push_back(i + 1);
+
+        // return ans;
+
+
+
+        // if(nums[0] != 1){
+        //     for(int i = 1; i<n; i++){
+        //         if(nums[i-1] == nums[i]){
+        //             ans.push_back(nums[i]);
+        //             ans.push_back(1);
+        //             return ans;
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i<n; i++){
+        //     if(nums[i] != i + 1){
+        //         ans.push_back(nums[i]);
+        //         for(int i = 1; i<=n ; i++){
+        //             if(nums[i] != i + 1){
+        //                 ans.push_back(nums[i]);
+        //                 return ans;
+        //             }
+        //         }
+        //     }
+        // }
+        // return {};
+        
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] == nums[i]) {
+                ans.push_back(nums[i]);  
+                break;
             }
         }
+
+        int idxsum = 0;
+        for(int i = 1; i<=n; i++){
+            idxsum += i;
+        }
         
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            sum += nums[i];
+        } 
+        ans.push_back(idxsum - sum + ans[0]);
         return ans;
     }
 };
