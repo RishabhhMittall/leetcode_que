228b3e8e073f320635f363c44bfe9ad2e2cd8da9
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-01-31 18:45:47 +0530

    Time: 22 ms (32.9%), Space: 27.2 MB (10.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,17 @@ public:
         vector<int> lres(n,1);
         vector<int> rres(n,1);
 
-        int lans = 1;
+        int lans = 1,rans = 1;
         for(int i = 1; i<n; i++){
             lans *= nums[i-1];
             lres[i] = lans;
         }
 
-        int rans = 1;
         for(int i = n-2; i >= 0; i--){
             rans *= nums[i+1];
             rres[i] = rans;
         }
+        
         vector<int> res(n);
         for(int i = 0; i<n; i++){
             res[i] = lres[i] * rres[i];
