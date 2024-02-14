292e83e727a3358e426e4536fda418a08477d8b1
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-02-14 21:52:38 +0530

    Time: 0 ms (100%), Space: 8.7 MB (21.63%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,34 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int cnt = 0;
+//         ListNode* temp = head;
+//         while(temp != NULL) {
+//             cnt++;
+//             temp = temp -> next;
+//         }
+//         // if(cnt % 2 == 0){
+//             cnt = (cnt/2);
+//             ListNode* one = head;
+//             while(cnt != 0){
+//                 one = one -> next;
+//                 cnt--;
+//             }
+//             return one;
+//         // }
+//         // else{
+//         //     cnt = (cnt/2);
+//         //     ListNode* one = head;
+//         //     while(cnt != 0){
+//         //         one = one -> next;
+//         //         cnt--;
+//         //     }
+//         //     return one;
+
+//         // }
+//         return head;
+//     }
+// };
+
+
+//--- approach 2 khiladi ---
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int cnt = 0;
-        ListNode* temp = head;
-        while(temp != NULL) {
-            cnt++;
-            temp = temp -> next;
-        }
-        // if(cnt % 2 == 0){
-            cnt = (cnt/2);
-            ListNode* one = head;
-            while(cnt != 0){
-                one = one -> next;
-                cnt--;
-            }
-            return one;
-        // }
-        // else{
-        //     cnt = (cnt/2);
-        //     ListNode* one = head;
-        //     while(cnt != 0){
-        //         one = one -> next;
-        //         cnt--;
-        //     }
-        //     return one;
+    
+        ListNode * one = head;
+        ListNode * two = head;
 
-        // }
-        return head;
+        while(one != NULL && one -> next != NULL){
+            one = one -> next -> next;
+            two = two -> next;
+        }
+
+        return two;
+
     }
-};
+};        
