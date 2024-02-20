97bf107e9d179eb65be4218aec24b439e8bc03d6
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-02-20 19:14:00 +0530

    Time: 3 ms (17.43%), Space: 7.3 MB (14.52%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
+    bool check(int n, int base){
+        string str="";
+        while(n>=1){
+            str+=to_string(n%base);
+            n/=base;
+
+        }
+        int i=0, j=str.length()-1;
+        while(i<=j){
+            if(str[i]!=str[j])return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     bool isStrictlyPalindromic(int n) {
-        return false;
+        bool res=check(n,2);
+        for(int i=3;i<=n-2;i++){
+            res=res && check(n,i);
+        }
+        return res;
+        
     }
 };
