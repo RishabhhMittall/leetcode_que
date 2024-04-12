8352d7b21bc9a8448dcc194f660002fe55b87fa9
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-04-12 11:58:17 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.24%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,10 +3,13 @@ public:
     int lengthOfLastWord(string s) {
         int n=s.size();
         int cnt=0,a=0;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]==' '&& a == 1)break;
-            if(s[i]!=' '){
-                a=1;
+        for(int i=n-1; i>=0 ; i--){
+            if(s[i] ==' ' &&  a == 1){
+                break;
+
+            }
+            if(s[i] != ' '){
+                a =1;
                 cnt++;
             }
         }
