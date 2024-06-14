d99260c86986e5b4b1b2afbf9911278789ab04d3
RishabhhMittall <155389183+RishabhhMittall@users.noreply.github.com>
2024-06-14 20:53:11 +0530

    Time: 27 ms (32.96%), Space: 22.7 MB (68.11%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int rows = intervals.size();
-        vector<vector<int>> res;
-                          
-        if (intervals.empty()) {
-            return res;
-        }
-
+        vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
-        
-        int ptr1 = intervals[0][0];
-        int ptr2 = intervals[0][1];
-                          
-        for(int i = 1 ; i < rows ; i++){
-            int currentStart = intervals[i][0];
-            int currentEnd = intervals[i][1];
 
-            if (currentStart <= ptr2) {
-                // Merge intervals
-                ptr2 = max(ptr2, currentEnd);
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            vector<int>& last = ans.back();                   //ans.back() returns last element in the interval
+            if (intervals[i][0] <= last[1]) {
+                last[1] = max(last[1], intervals[i][1]);
+                //  ans.back() = last;                     // for passing without refrence 
             } else {
-                // Add the merged interval to the result and update ptrs
-                res.push_back({ptr1, ptr2});
-                ptr1 = currentStart;
-                ptr2 = currentEnd;
+                ans.push_back(intervals[i]);
             }
         }
-        
-        res.push_back({ptr1,ptr2});
-        
-        return res;
+
+        return ans;
     }
 };
