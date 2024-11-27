802dfc517e14e8039283399faeeb5dd48bbd3127
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2024-11-27 22:28:08 +0530

    Time: 47 ms (77.66%), Space: 29 MB (46.23%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        // vector<int> temp;
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        for(int i =0; i<n-1; i++){
-            if(i==0 || (i>0 && nums[i] != nums[i-1])){
-                int l = i+1;
-                int r = n-1;
-            
-                while(l<r ){
-                    int sum = nums[i] + nums[l] + nums[r] ;
-                    if( sum == 0){
-                        ans.push_back({nums[i],nums[l],nums[r]});
-                        while (l < r && nums[l] == nums[l + 1]) l++; 
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        l++;
-                        r--;
-                    }
-                    else if(sum < 0){
-                        l++;
-                    }
-                    else{
-                        r--;
-                    }
 
+        for(int i = 0; i<nums.size(); i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+
+                int j = i + 1;
+                int k = nums.size() - 1;
+                
+                while(j < k) {
+                    int sum = nums[i] + nums[j] + nums[k]; 
+                    if(sum < 0) {
+                        j++;
+                    }
+                    else if (sum > 0) {
+                        k--;
+                    }
+                    else {
+                        ans.push_back({nums[i], nums[j] , nums[k]});
+                        while(j<k && nums[j] == nums[j+1]) j++;
+                        while(j<k && nums[k] == nums[k-1]) k--;
+                        j++ ;
+                        k--;
+                    }
                 }
             }
         }
