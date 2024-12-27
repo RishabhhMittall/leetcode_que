355347f040c731b513eb48351ca4e5a23de8916f
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2024-12-27 10:41:40 +0530

    Time: 284 ms (5.09%), Space: 10.8 MB (99.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int idx1 = -1;
-        int idx2 = -1;
-        for(int i = 0 ; i<nums.size()-1; i++) {
-            for(int j = i+1; j<nums.size(); j++) {
-                if(nums[i] + nums[j] == target){
-                    idx1 = i;
-                    idx2 = j;
+    vector<int> twoSum(vector<int> nums, int target) {
+        int n=nums.size();
+        for(int i = 0; i < n; i++){
+            for(int j =i+1; j < n; j++){
+                if(nums[i]+nums[j]==target){
+                    return{i,j};
                 }
-            } 
+            }
         }
-        return {idx1,idx2};
-        //comment learning git
+        return {};
+        
     }
 };
