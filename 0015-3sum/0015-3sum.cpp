612fe6e40adf50d5495c49afcc268bc3306fe1d0
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-01-17 23:42:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,27 +5,29 @@ public:
         vector<vector<int>> ans;
 
         for(int i = 0; i<nums.size(); i++) {
-            if(i == 0 || (i > 0 && nums[i] != nums[i-1])) {
+            if(i == 0 ||(i>0 && (nums[i] != nums[i-1]))) {
 
-                int j = i + 1;
+                int j = i+1;
                 int k = nums.size() - 1;
-                
-                while(j < k) {
-                    int sum = nums[i] + nums[j] + nums[k]; 
-                    if(sum < 0) {
+
+                int sum = nums[i] + nums[k] + nums[j];
+
+                while(j<k) {
+                    if(sum > 0) {
+                        k--;
+                    }
+                    else if( sum < 0) {
                         j++;
                     }
-                    else if (sum > 0) {
-                        k--;
-                    }
                     else {
-                        ans.push_back({nums[i], nums[j] , nums[k]});
+                        ans.push_back({nums[i], nums[j], nums[k]});
                         while(j<k && nums[j] == nums[j+1]) j++;
                         while(j<k && nums[k] == nums[k-1]) k--;
-                        j++ ;
+                        j++;
                         k--;
+
                     }
-                }
+                }    
             }
         }
         return ans;
