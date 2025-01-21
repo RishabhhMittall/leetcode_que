15a48eee827df1db857fd2ba9bd83baaa2fcccc4
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-01-21 22:17:56 +0530

    Time: 3 ms (10.59%), Space: 14 MB (100%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -1,15 +1,28 @@
+// class Solution {
+// public:
+//     int countSeniors(vector<string>& details) {
+//         int n = details.size();
+//         int count = 0;
+
+//         for(int i = 0; i< n ; i++ ) {
+            
+//             string old = details[i].substr(11,2);
+//             int age = stoi(old);
+//             if(age > 60) { 
+//                 count ++;
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        int n = details.size();
         int count = 0;
-
-        for(int i = 0; i< n ; i++ ) {
-            string old = details[i].substr(11,2);
-            int age = stoi(old);
-            if(age > 60) { 
-                count ++;
-            }
+        for(int i=0;i<details.size();i++){
+            if((details[i][11]>='6' && details[i][12]>'0')|| details[i][11]>='7')
+            count++;
         }
         return count;
     }
