4d203926959e67cf53454db497d227bb288569ea
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-01-23 22:54:17 +0530

    Time: 1 ms (46.74%), Space: 9.9 MB (53.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
 
-    char toLower (char ch) {
+    char toLower(char ch) {
         if(ch >= 'A' && ch <= 'Z') {
             return ch - 'A' + 'a';
         }
         return ch;
     }
 
-    bool alphaNumeric (char ch) {
-        if( ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z') {
+    bool isAlphaNumeric(char ch) {
+        if(ch >= '0' && ch <= '9' || ch >= 'a' && ch <= 'z') {
             return true;
         }
-        return false;
+            return false;
     }
 
+
     bool isPalindrome(string s) {
-        int i  = 0;
-        int j = s.length() -1;
+        int n = s.length();
 
-        while(i<=j) {
-            char l = toLower(s[i]);
-            char r = toLower(s[j]);
+        int i = 0;
+        int j = n - 1;
+        
+        while(i<j) {
+            char left = toLower(s[i]);
+            char right = toLower(s[j]);
 
-            if( !alphaNumeric(l)) {
+            if( isAlphaNumeric(left) != true) {
                 i++;
             }
-            else if(!alphaNumeric(r)) {
+            else if( isAlphaNumeric(right) != true) {
                 j--;
             }
-
-            else if(l != r) {
+            else if(left != right) {
                 return false;
-        
             }
             else {
                 i++;
