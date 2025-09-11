7aeb4326b55024c88b8d59bba4ef125b5aa20486
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-09-11 20:27:58 +0530

    Time: 2 ms (82.06%), Space: 18.3 MB (56.91%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
     int similarPairs(vector<string>& words) {
-        vector<set<char>> st;
-
-        for(int i = 0; i < words.size(); i++) {
-            string curr = words[i];
-            set<char>temp;
-            for(int j = 0; j<curr.length(); j++) {
-                temp.insert(curr[j]);
-            }
-            st.push_back(temp);
-            temp.clear();
-        }
-
+        unordered_map<string, int> freq;
         int ans = 0;
-        int n = st.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (st[i] == st[j]) {
-                    ans++;
-                }
+
+        for (string &word : words) {
+            // collect unique chars
+            vector<int> seen(26, 0);
+            for (char c : word) {
+                seen[c - 'a'] = 1;
             }
+
+            // build signature string
+            string sig = "";
+            for (int i = 0; i < 26; i++) {
+                if (seen[i]) sig.push_back('a' + i);
+            }
+
+            ans += freq[sig];  // count how many same signatures appeared before
+            freq[sig]++;
         }
+
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int similarPairs(vector<string>& words) {
+//         vector<set<char>> st;
+
+//         for(int i = 0; i < words.size(); i++) {
+//             string curr = words[i];
+//             set<char>temp;
+//             for(int j = 0; j<curr.length(); j++) {
+//                 temp.insert(curr[j]);
+//             }
+//             st.push_back(temp);
+//             temp.clear();
+//         }
+
+//         int ans = 0;
+//         int n = st.size();
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (st[i] == st[j]) {
+//                     ans++;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
