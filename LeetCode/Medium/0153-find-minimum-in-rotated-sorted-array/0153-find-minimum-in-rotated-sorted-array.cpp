e82efbf4466859d68ffa7ee79cb13725201bb362
Rishabh Mittal <155389183+RishabhhMittall@users.noreply.github.com>
2025-10-02 21:43:55 +0530

    0153-find-minimum-in-rotated-sorted-array 10-02-2025 Medium

--- LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -4,13 +4,33 @@ public:
         
         int n = nums.size();
 
-        for(int i = 0; i<n-1; i++) {
-            if(nums[i] > nums[i+1]) {
-                return  nums[i+1];
+        // for(int i = 0; i<n-1; i++) {
+        //     if(nums[i] > nums[i+1]) {
+        //         return  nums[i+1];
+        //     }
+        // }
+
+        // return nums[0];
+
+        int low = 0, high = n - 1;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+
+           
+            if (nums[mid] < nums[high]) {
+                // in left
+                high = mid;
+            } 
+            else if (nums[mid] > nums[high]) {
+                // in right
+                low = mid + 1;
+            } 
+            else {
+                high--;
             }
         }
 
-        return nums[0];
-
+        return nums[low];
     }
 };
